35461afae75687cac596d3611877f936cdc47e6d
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-08 13:32:22 +0800

    added AdminNav component

--- panay-app/src/App.vue
+++ panay-app/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import AdminNav from './components/admin/AdminNav.vue'
 </script>
 
 <template>
   <header>  
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <AdminNav />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
