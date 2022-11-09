41da4ee87a01c4505e926837ce404a15d2103381
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-09 18:59:32 +0800

    Added hover effects on sidebar items, more design refining

--- panay-app/src/components/admin/AdminNav.vue
+++ panay-app/src/components/admin/AdminNav.vue
@@ -3,38 +3,44 @@
 </script>
 
 <template>
-    <nav id="admin-sidebar" class="col-md-3 col-lg-2 sidebar text-white bg-primary">
-        <span class="fs-4">Panay Tinapay</span>
+    <nav id="admin-sidebar" class="col-md-3 col-lg-2 p-0 sidebar text-white bg-primary">
+        <span class="p-3 fs-4">Panay Tinapay</span>
         <hr>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/a">Dashboard</RouterLink>
-        </div>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/records">Records</RouterLink>
-        </div>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/reports">Reports</RouterLink>
-        </div>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/branches">View Branches</RouterLink>
-        </div>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/cheques">Cheques</RouterLink>
-        </div>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/editcategories">Categories</RouterLink>
-        </div>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/a/account">Account</RouterLink>
-        </div>
-        <div class="nav-item">
-            <RouterLink class="nav-link" to="/logout">Logout</RouterLink>
-        </div>        
+
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/a">Dashboard</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/records">Records</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/reports">Reports</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/branches">View Branches</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/cheques">Cheques</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/editcategories">Categories</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/a/account">Account</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link fw-bold" to="/logout">Logout</RouterLink>
+            </li>  
+        </ul>      
     </nav>
 
 
 </template>
 
-<style>
-
+<style scoped>
+    .nav-item:hover {
+        background-color: #CC7066!important;
+        transition: 0.3s!important;
+    }
 </style>
