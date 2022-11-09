b49704f64cdf91946b348b5525222219374e618c
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2022-11-10 01:58:39 +0800

    Implement pwd hashing on signup

--- controller/controller.js
+++ controller/controller.js
@@ -1,27 +1,33 @@
 var path = require('path');
 const dbUser = require('../models/dbUser.js');
+const bcrypt = require('bcrypt');
 
 const controller = {
     getIndex: (req, res) => {
         res.sendFile(path.resolve('./views/login.html'))
     },
-    
-    
+
+
     login: (req, res) => {
 
-       
+
     },
-    signup: (req,res) => {
-        const {name, password, isadmin} = req.body;
+    signup: (req, res) => {
+        const { name, password, isadmin } = req.body;
         console.log(req.body)
-        dbUser.User.create({
-             
-            branchName:     name,
-            branchPassword: password,
-            isAdmin:        isadmin,
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, function (err, hashed) {
+            if (err) {
+                console.log(err)
+            } else {
+                dbUser.User.create({
+                    branchName: name,
+                    branchPassword: hashed,
+                    isAdmin: isadmin,
+                })
+                res.redirect('/');
+            }
         })
-        res.redirect('/');
-        
     }
 
 }
