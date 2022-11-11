644184e932789b392dc89bb09d22a903d6038f90
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-11 23:31:59 +0800

    removed unused imports

--- panay-app/src/App.vue
+++ panay-app/src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import AdminNav from './components/admin/AdminNav.vue'
 </script>
 
 <template>
