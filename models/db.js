9a3eb4ac70b101e816ee6e3b6341bd90aa7c1e27
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2022-11-11 15:55:50 +0800

    Create single db.js handler
    Switching to one database.

---
+++ models/db.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+const AdminExpenseSchema = require('./admin/adminExpenseSchema');
+const AdminSalesSchema = require('./admin/adminSalesSchema');
+const AdminCategorySchema = require('./admin/AdminCategorySchema');
+const ExpenseSchema = require('./branch/expenseSchema');
+const SalesSchema = require('./branch/salesSchema');
+const UserSchema = require('./UserSchema');
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/CSSWENG-Team-3';
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+const database = {
+
+    connect: function () {
+        mongoose.connect(url, options, function (error) {
+            if (error) throw error;
+            console.log('Connected to: ' + url);
+        });
+    },
+
+    insertOne: function (model, doc, callback) {
+        model.create(doc, function (error, result) {
+            if (error) return callback(false);
+            console.log('Added ' + result);
+            return callback(true);
+        });
+    },
+
+    insertMany: function (model, docs, callback) {
+        model.insertMany(docs, function (error, result) {
+            if (error) return callback(false);
+            console.log('Added ' + result);
+            return callback(true);
+        });
+    },
+
+    findOne: function (model, query, projection, callback) {
+        model.findOne(query, projection, function (error, result) {
+            if (error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    findMany: function (model, query, projection, callback) {
+        model.find(query, projection, function (error, result) {
+            if (error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    updateOne: function (model, filter, update, callback) {
+        model.updateOne(filter, update, function (error, result) {
+            if (error) return callback(false);
+            console.log('Document modified: ' + result.modifiedCount);
+            return callback(true);
+        });
+    },
+
+    updateMany: function (model, filter, update, callback) {
+        model.updateMany(filter, update, function (error, result) {
+            if (error) return callback(false);
+            console.log('Documents modified: ' + result.modifiedCount);
+            return callback(true);
+        });
+    },
+
+    deleteOne: function (model, conditions, callback) {
+        model.deleteOne(conditions, function (error, result) {
+            if (error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
+        });
+    },
+
+    deleteMany: function (model, conditions, callback) {
+        model.deleteMany(conditions, function (error, result) {
+            if (error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
+        });
+    }
+
+}
+
+module.exports = database;
