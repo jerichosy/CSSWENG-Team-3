cb11c61a4d4e80a7f437891b049363e4c105b3c5
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-12 01:15:18 +0800

    removed content-type header because it was causing req.body in the backend to be empty

--- panay-app/src/http-common.js
+++ panay-app/src/http-common.js
@@ -3,8 +3,5 @@ import axios from 'axios';
 const url = 'http://localhost:3000/';
 
 export default axios.create({
-    baseURL: url,
-    headers: {
-        "Content-type": "application/json"
-    }
+    baseURL: url
 });
