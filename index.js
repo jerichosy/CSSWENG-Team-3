edb45a4107694955eeb4e028252446b27f71a2c1
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2022-11-11 15:56:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const routes = require('./routes/routes.js');
+const db = require('./models/db.js');
 
 const app = new express();
 
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); //for req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //for req.body
 
 const dbUser = require('./models/dbUser.js');
 const dbBranch = require('./models/branch/dbBranch.js');
 const dbAdmin = require('./models/admin/dbAdmin.js');
 
-dbBranch.createConnect();
-dbUser.createConnect();
-dbAdmin.createConnect();
+// dbBranch.createConnect();
+// dbUser.createConnect();
+// dbAdmin.createConnect();
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Node Server is Running at Port 3000");
 });
 
 app.use('/', routes);
+
+db.connect();
