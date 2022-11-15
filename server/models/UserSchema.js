463884f4c0dbe5584ff507ff0012e3e50c2f4e43
Antoinne Bryce Salvador <antoinne_salvador@dlsu.edu.ph>
2022-11-15 18:08:46 +0800

    update userschema

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    branchID: { type: String, required: true, default: ' ' },
-    branchName: { type: String, required: true },
+    branchID: { type: String, required: true },
+    branchName: { type: String, required: true, default: ' ' },
     branchPassword: { type: String, required: true },
     isAdmin: { type: Boolean, required: true },
 }, { timestamps: true });
