e4614da12cfe36b952fa0377a2873d1d5539c415
Antoinne Bryce Salvador <antoinne_salvador@dlsu.edu.ph>
2022-11-15 17:53:33 +0800

    swap sales and expense schema

--- server/models/admin/adminSalesSchema.js
+++ server/models/admin/adminSalesSchema.js
@@ -1,11 +1,13 @@
+// TODO:
+// This might not be needed since we can just do "multiple module.exports" in the ExpenseSchema.js file. 
+// After all, they are exactly same schema, just diff. collection.
+
 const mongoose = require('mongoose');
 
-const AdminSalesSchema = new mongoose.Schema({
+const AdminExpenseSchema = new mongoose.Schema({
     branchID: { type: String, required: true },
-    item: { type: String, required: true },
-    category: { type: String, required: true },
-    amount: { type: Number, required: true },
-    notes: { type: String, required: false },
+    sales: { type: Number, required: true },
+    customercount: { type: String, required: false },
 }, { timestamps: true });
 
-module.exports = mongoose.model('AdminSales', AdminSalesSchema);
+module.exports = mongoose.model('AdminExpense', AdminExpenseSchema);
