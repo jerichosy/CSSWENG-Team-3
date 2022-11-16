3e160d0aedb64296b5ab2fd64bcf4a34c113bef1
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-16 19:54:26 +0800

    added placeholder data for sales

--- panay-app/src/views/admin/adminpages/records/SalesView.vue
+++ panay-app/src/views/admin/adminpages/records/SalesView.vue
@@ -21,6 +21,258 @@
                 <td>9,999,999</td>
                 <td>Lorem ipsum dolor sit vivamus.</td>
             </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
+            <tr>
+                <td>2022/01/01</td>
+                <td>08:00 PM</td>
+                <td>P9,999,999,999.99</td>
+                <td>9,999,999</td>
+                <td>Lorem ipsum dolor sit vivamus.</td>
+            </tr>
         </tbody>
     </table>
 </div>
