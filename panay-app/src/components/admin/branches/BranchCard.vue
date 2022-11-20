2f3d9daad4d8cb19ec39e0083a06b8e64431a9b1
andres-dalisay <apadx3304@gmail.com>
2022-11-20 17:46:21 +0800

    layouts for delete and edit buttons

--- panay-app/src/components/admin/branches/BranchCard.vue
+++ panay-app/src/components/admin/branches/BranchCard.vue
@@ -2,15 +2,30 @@
 export default {
     props: {
         branch: Object
+    },
+
+    methods: {
+
     }
+
+
 }
 </script>
 
 <template>
-    <div class="col-lg-4 col-sm-6 p-2 border border-2">
-        <div class="container p-3 m-0 border rounded-3 bg-primary text-white">
-            <h6>{{branch.branchID}}</h6>
-            <h5>{{branch.branchName}}</h5>
+    <div class="col-lg-4 col-sm-6 container border border-2 p-3">
+        <div class="row h-50">
+            <div class="text-start col-8 p-3 m-0 border border-0 rounded-start bg-primary text-white">
+                <div class="row">
+                    <div class="col-8">
+                        <h6>{{branch.branchID}}</h6>
+                        <h5>{{branch.branchName}}</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 m-0 rounded-end bg-info">
+                    
+            </div>
         </div>
     </div>
 </template>
