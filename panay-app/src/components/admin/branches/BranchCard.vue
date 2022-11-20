de66482531e5786c3f2f8f166795ee77a81cf392
andres-dalisay <apadx3304@gmail.com>
2022-11-20 16:41:14 +0800

    made branch displays

---
+++ panay-app/src/components/admin/branches/BranchCard.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: {
+        branch: Object
+    }
+}
+</script>
+
+<template>
+    <div class="col-lg-4 col-sm-6 p-2 border border-2">
+        <div class="container p-3 m-0 border rounded-3 bg-primary text-white">
+            <h6>{{branch.branchID}}</h6>
+            <h5>{{branch.branchName}}</h5>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- panay-app/src/views/admin/adminpages/Branches.vue
+++ panay-app/src/views/admin/adminpages/Branches.vue
@@ -1,7 +1,29 @@
-<script setup>
+<script>
+import BranchCard from '../../../components/admin/branches/BranchCard.vue'
 
+export default {
+    data() {
+        return {
+            branches: [{branchID: 101, branchName: 'Paligsahan QC'}, {branchID: 102, branchName: 'Pag-asa QC'}, {branchID: 103, branchName: 'Tandang Sora QC'}]
+        }
+    },
+    components: {
+        BranchCard
+    }
+}
 </script>
 
 <template>
-    <span>Branches</span>
+    <div class="container-fluid">
+        <div class="row border border-2">
+            <h1>Manage Branches</h1>
+        </div>
+        <div class="row border border-2">
+            <button class="col-2">Add</button>
+        </div>
+        <div class="row container m-1 p-1">
+            <BranchCard v-for="singleBranch in branches" :branch="singleBranch"/>
+        </div>
+    </div>
+
 </template>
