4dde12af63c0426bc363ed5f8294b0481dd0b1da
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-24 22:16:44 +0800

    removed commented code

--- panay-app/src/components/admin/records/FilterOptions.vue
+++ panay-app/src/components/admin/records/FilterOptions.vue
@@ -8,8 +8,8 @@ export default {
     },
 
     props: {
-        branchOptions: {type: Array},
-        categoryOptions: {type: Array}
+        branchOptions: { type: Array },
+        categoryOptions: { type: Array }
     },
 
     emits: ['updateFilters'],
@@ -31,7 +31,7 @@ export default {
     watch: {
         filters: {
             handler(newFilter) {
-                    this.updateFilter(newFilter)
+                this.updateFilter(newFilter)
             },
             deep: true
         }
@@ -51,19 +51,19 @@ export default {
             let endDate = this.formatDate(this.endDate)
         }
     },
-    
+
     methods: {
         updateFilter(newFilter) {
             return this.$emit('updateFilters', newFilter)
         },
-        resetFilters(){
-            this.filters.dateFrom='',
-            this.filters.dateTo='',
-            this.filters.timeFrom='',
-            this.filters.timeTo='',
-            this.filters.itemSearch='',
-            this.filters.checkedBranches=[],
-            this.filters.checkedCategories=[]
+        resetFilters() {
+            this.filters.dateFrom = '',
+                this.filters.dateTo = '',
+                this.filters.timeFrom = '',
+                this.filters.timeTo = '',
+                this.filters.itemSearch = '',
+                this.filters.checkedBranches = [],
+                this.filters.checkedCategories = []
         }
     }
 }
@@ -74,25 +74,29 @@ export default {
         <div class="card card-body">
 
             <fieldset v-if="isExpenses">
-                <label for="item-search">Item Search</label> <input id="item-search" type="text" v-model="filters.itemSearch"/>
+                <label for="item-search">Item Search</label> <input id="item-search" type="text"
+                    v-model="filters.itemSearch" />
             </fieldset>
 
             <fieldset v-if="isSales || isExpenses">
                 <legend>Date Range</legend>
-                <label for="date-from">From</label> <input type="date" name="date-from" id="date-from" v-model="filters.dateFrom">
+                <label for="date-from">From</label> <input type="date" name="date-from" id="date-from"
+                    v-model="filters.dateFrom">
                 <label for="date-to">To</label> <input type="date" name="date-to" id="date-to" v-model="filters.dateTo">
             </fieldset>
 
             <fieldset v-if="isSales">
                 <legend>Time Range</legend>
-                <label for="time-from">From</label> <input type="time" name="time-from" id="time-from" v-model="filters.timeFrom" >
-                <label for="time-to">To</label> <input type="time" name="time-to" id="time-to" v-model="filters.timeTo" >
+                <label for="time-from">From</label> <input type="time" name="time-from" id="time-from"
+                    v-model="filters.timeFrom">
+                <label for="time-to">To</label> <input type="time" name="time-to" id="time-to" v-model="filters.timeTo">
             </fieldset>
 
             <fieldset v-if="isSales || isExpenses">
                 <legend>Branches</legend>
                 <div v-for="branch in branchOptions">
-                    <input type="checkbox" name="branches" :id="branch.branchName.toLowerCase()" :value="branch.branchName" v-model="filters.checkedBranches" />
+                    <input type="checkbox" name="branches" :id="branch.branchName.toLowerCase()"
+                        :value="branch.branchName" v-model="filters.checkedBranches" />
                     <label :for="branch.branchName.toLowerCase()">{{branch.branchName}}</label>
                 </div>
             </fieldset>
@@ -100,7 +104,8 @@ export default {
             <fieldset v-if="isExpenses">
                 <legend>Categories</legend>
                 <div v-for="category in categoryOptions">
-                    <input type="checkbox" name="branches" :id="category.name.toLowerCase()" :value="category.name" v-model="filters.checkedCategories" />
+                    <input type="checkbox" name="branches" :id="category.name.toLowerCase()" :value="category.name"
+                        v-model="filters.checkedCategories" />
                     <label :for="category.name.toLowerCase()">{{category.name}}</label>
                 </div>
             </fieldset>
@@ -108,30 +113,4 @@ export default {
             <button type="reset" @click="resetFilters">Reset Filters</button>
         </div>
     </form>
-
-    <!-- expense filters -->
-    <!-- <div class="collapse" id="filterForm" v-if="isExpenses">
-        <div class="card card-body">
-            
-            <label for="item-search">Item Search</label> <input id="item-search" type="text" v-model="filters.itemSearch">
-            <fieldset>
-                <legend>Date Range</legend>
-                <label for="date-from">From</label> <input type="date" name="date-from" id="date-from" v-model="filters.dateFrom">
-                <label for="date-to">To</label> <input type="date" name="date-to" id="date-to" v-model="filters.dateTo">
-               
-            </fieldset>
-            
-            <FilterCheckboxDropdown filter-item-single="branch"
-                                    filter-item-plural="branches"
-                                    :options="this.branchOptions"
-                />
-
-            <FilterCheckboxDropdown filter-item-single="category"
-                                filter-item-plural="categories"
-                                :options="this.categoryOptions"
-            />
-        </div>
-    </div> -->
-
-
 </template>
