62f136ccb757197434a394d957081127cdd7e2b4
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-25 02:54:42 +0800

    added delete api calls for sales and expenses

--- panay-app/src/services/RecordService.js
+++ panay-app/src/services/RecordService.js
@@ -8,6 +8,16 @@ class RecordService {
     getAdminExpenses() {
         return http.get("/adminviewexpense");
     }
+
+    deleteAdminSales(data) {
+        console.log(data)
+        return http.post("/admindeletesales", data);
+    }
+
+    deleteAdminExpense(data) {
+        console.log(data)
+        return http.post("/admindeleteexpense", data);
+    }
 }
 
 export default new RecordService();
