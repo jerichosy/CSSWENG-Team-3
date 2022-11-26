d836debe08a6a22519cbfa38991a4740d9f9bf83
Antoinne Bryce Salvador <antoinne_salvador@dlsu.edu.ph>
2022-11-26 16:05:00 +0800

    delete old schema

--- server/models/admin/adminExpenseSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminExpenseSchema = new mongoose.Schema({
-    branchID: { type: String, required: true },
-    sales: { type: Number, required: true },
-    customercount: { type: String, required: false },
-}, { timestamps: true });
-
-module.exports = mongoose.model('AdminExpense', AdminExpenseSchema);
