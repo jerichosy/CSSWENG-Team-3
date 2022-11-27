77ce96fd9573289fa659985e807d7e5480a73e59
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-28 03:12:40 +0800

    finished (redesigned) filter menu for cheques

--- panay-app/src/views/admin/adminpages/records/ChequesView.vue
+++ panay-app/src/views/admin/adminpages/records/ChequesView.vue
@@ -98,11 +98,80 @@ export default {
         </div>
     </div>
 
-    <div class="row justify-content-end m-0 p-2">
-        <div class="col-auto p-0 ms-2">
-            <button class="btn btn-primary">Filter</button>
+    <!-- Filter Menu -->
+    <div class="row m-0 p-2">
+        <div class="col-5 me-auto p-0">
+            <div class="dropdown">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
+                    aria-expanded="false" data-bs-auto-close="outside">
+                    Filters
+                </button>
+                <form id="filter-dropdown" class="dropdown-menu p-4">
+                    <fieldset class="mb-2">
+                        <legend class="col-form-label-sm m-0">Date Range</legend>
+                        <div class="row">
+                            <div class="col">
+                                <input type="date" title="from" class="form-control" id="filterDateFrom" />
+                            </div>
+                            <div class="col">
+                                <input type="date" title="to" class="form-control" id="filterDateTo" />
+                            </div>
+                        </div>
+
+
+                    </fieldset>
+
+                    <div class="mb-2">
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="filterAccount" placeholder="Account" />
+                            <label for="filterAccount">Search for an account</label>
+                        </div>
+                    </div>
+
+                    <div class="mb-2">
+                        <div class="row">
+                            <div class="col-auto dropdown">
+                                <button type="button" class="btn btn-outline-primary dropdown-toggle"
+                                    data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                                    Branches
+                                </button>
+                                <div id="branch-dropdown" class="dropdown-menu p-4">
+                                    <div class="form-check" v-for="branch in branchOptions">
+                                        <input class="form-check-input btn-check" type="checkbox" name="branches"
+                                            :id="'branch' + branch.branchID" :value="branch.branchName" />
+                                        <label class="form-check-label btn btn-outline-primary text-nowrap"
+                                            :for="'branch' + branch.branchID">{{ branch.branchName }}</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-auto dropdown">
+                                <button type="button" class="btn btn-outline-primary dropdown-toggle"
+                                    data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                                    Categories
+                                </button>
+                                <div id="category-dropdown" class="dropdown-menu p-4">
+                                    <div class="form-check" v-for="category in categoryOptions">
+                                        <input class="form-check-input btn-check" type="checkbox" name="branches"
+                                            :id="category.name.toLowerCase()" :value="category.name" />
+                                        <label class="form-check-label btn btn-outline-primary text-nowrap"
+                                            :for="category.name.toLowerCase()">{{ category.name }}</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <button type="reset" class="btn btn-primary">Reset Filters</button>
+                </form>
+            </div>
         </div>
-        <div class="col-auto p-0 ms-2">
+
+
+
+        <div class="col-auto p-0">
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">Add</button>
         </div>
     </div>
@@ -134,3 +203,12 @@ export default {
         </div>
     </div>
 </template>
+
+<style scoped>
+#branch-dropdown,
+#category-dropdown {
+    max-height: 280px !important;
+    min-width: auto !important;
+    overflow-y: auto !important;
+}
+</style>
