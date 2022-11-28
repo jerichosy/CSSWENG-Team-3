68f69cb04c85b4665165160fe211f0210c55b73b
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-29 00:36:18 +0800

    removed an unneeded comma

--- panay-app/src/components/admin/records/cheques/ChequesFilters.vue
+++ panay-app/src/components/admin/records/cheques/ChequesFilters.vue
@@ -20,7 +20,7 @@ export default {
                 accountSearch: '',
                 checkedBranches: [],
                 checkedCategories: []
-            }
+            },
         }
     },
 
