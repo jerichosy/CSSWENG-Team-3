b19e2e204a694a6f5ca4aa12d05e4e6b58943ac1
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-30 12:25:00 +0800

    removed unnecessary filter property

--- panay-app/src/views/admin/adminpages/records/SalesView.vue
+++ panay-app/src/views/admin/adminpages/records/SalesView.vue
@@ -26,7 +26,6 @@ export default {
                 dateTo: '',
                 timeFrom: '',
                 timeTo: '',
-                itemSearch: '',
                 checkedBranches: [],
                 checkedCategories: []
             },
