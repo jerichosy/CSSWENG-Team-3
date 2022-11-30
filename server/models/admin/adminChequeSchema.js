36074bf68ad2b1b58582134af97136d997be59c4
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-11-30 18:45:50 +0800

    added branchID and branchName to adminChequeSchema

--- server/models/admin/adminChequeSchema.js
+++ server/models/admin/adminChequeSchema.js
@@ -4,7 +4,9 @@ const AdminChequeSchema = new mongoose.Schema({
     datetime: { type: Date, required: true },
     account: { type: String, required: true },
     category: { type: String, required: true },
-    amount: { type: Number, required: true }
+    amount: { type: Number, required: true },
+    branchID: { type: String, required: true },
+    branchName: { type: String, required: true, default: ' ' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('AdminCheque', AdminChequeSchema);
