69fc9fe92232d2605071f6934b17b253d62e366a
andres-dalisay <apadx3304@gmail.com>
2022-12-02 14:29:19 +0800

    edited layout for branch cards

--- panay-app/src/components/admin/branches/BranchCard.vue
+++ panay-app/src/components/admin/branches/BranchCard.vue
@@ -13,15 +13,15 @@ export default {
 </script>
 
 <template>
-    <div class="col-lg-4 col-sm-6 p-3">
-        <div class="card border-primary" style="width: 18rem;">
-            <div class="card-header bg-primary text-white">
+    <div class="col-lg-4 col-sm-6 p-3 d-flex justify-content-center">
+        <div class="card border-primary" style="height: 10rem; width: 18rem;">
+            <div class="card-header bg-primary text-white" style="height: 6rem;">
                 <h6>{{ branch.branchID }}</h6>
                 <h5>{{ branch.branchName }}</h5>
             </div>
 
 
-            <div class="p-3 m-0">
+            <div class="p-2 m-1">
                 <button class="btn btn-secondary text-white me-2">Edit</button>
                 <button class="btn btn-danger">Delete</button>
             </div>
--- panay-app/src/views/admin/adminpages/Branches.vue
+++ panay-app/src/views/admin/adminpages/Branches.vue
@@ -32,13 +32,16 @@ export default {
 </script>
 
 <template>
-    <div class="row border border-2">
-        <h1>Manage Branches</h1>
+    <div class="row">
+        <h1 class="p-3 m-0">Branches</h1>
+        <hr />
     </div>
-    <div class="row border border-2">
-        <button class="col-2">Add</button>
+    <div class="row justify-content-end ps-3 pe-3 mb-2">
+        <div class="col-auto">
+            <button class="btn btn-primary">Add</button>
+        </div>
     </div>
-    <div class="row justify-content-evenly m-1 p-1">
+    <div class="row ps-2 pe-2">
         <BranchCard v-for="branch in branches" :branch="branch" />
     </div>
 </template>
