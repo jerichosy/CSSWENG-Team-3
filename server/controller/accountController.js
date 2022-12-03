44d9d82f3b01294df96a04bc25fa669fd24a4b61
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2022-12-04 05:58:10 +0800

    Generate branchID implemented
    Need to finalize branchID numbering though because this doesn't perform checks

--- server/controller/accountController.js
+++ server/controller/accountController.js
@@ -71,21 +71,34 @@ const authController = {
                 res.status(409).json({ msg: 'The specified username already exists.' });  //409 conflict
             }
             else {
-                const saltRounds = 10;
-                bcrypt.hash(password, saltRounds, function (err, hashed) {
-                    var user = {
-                        branchID: name,
-                        branchPassword: hashed,
+                // FIXME: Finalize issues with branchID numbering
+                // find the last branchID and increment it by 1
+                // this assumes that the branchIDs are in ascending order 
+                // db.findMany(User, {}, 'branchID', { sort: { branchID: -1 }, limit: 1 }, (result) => {
+                db.findMany(User, {}, 'branchID', (result) => {
+                    console.log(result);
+                    var branchID = 1;
+                    if (result.length >= 1) {
+                        branchID = parseInt(result[result.length - 1].branchID) + 1;
                     }
 
-                    db.insertOne(User, user, function (flag) {
-                        if (flag) {
-                            console.log('Sign up successful');
-                            res.status(201).json({ msg: 'Sign Up Successful' });
-                        } else {
-                            console.log('Sign up failed');
-                            res.status(400).json({ msg: 'Something went wrong. Please try again.' })
+                    const saltRounds = 10;
+                    bcrypt.hash(password, saltRounds, function (err, hashed) {
+                        var user = {
+                            branchID: branchID,
+                            branchName: name,
+                            branchPassword: hashed,
                         }
+
+                        db.insertOne(User, user, function (flag) {
+                            if (flag) {
+                                console.log('Sign up successful');
+                                res.status(201).json({ msg: 'Sign Up Successful' });
+                            } else {
+                                console.log('Sign up failed');
+                                res.status(400).json({ msg: 'Something went wrong. Please try again.' })
+                            }
+                        })
                     })
                 })
             }
