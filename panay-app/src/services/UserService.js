4715afc4dd004473c45c7dc65257e8a373525959
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-12-05 15:22:09 +0800

    added prototype methods for changing branch password and validating a password

--- panay-app/src/services/UserService.js
+++ panay-app/src/services/UserService.js
@@ -9,6 +9,26 @@ class UserService {
         return http.post("/deletebranch", data);
     }
 
+    changeBranchPassword(data) {
+        console.log(data);
+        // return http.post("/changebranchpassword", data);
+    }
+
+    validatePassword(data) {
+        // TODO: 
+        // give: user _id and password
+        // retrieve: boolean whether password is correct or not
+        const adminRecord = {
+            "_id": 1234,
+            "password": 'asdf'
+        }
+
+        if (data._id === adminRecord._id && data.password === adminRecord.password) {
+            return true
+        }
+        return false
+    }
+
     signup(data) {
         return http.post("/signup", data);
     }
