405a19b6d9b3a75e977e1741d33f923e11acf57d
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2022-12-07 10:57:14 +0800

    Added comments in routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,9 @@ const adminController = require('../controller/adminController.js');
 const branchController = require('../controller/branchController.js');
 const accountController = require('../controller/accountController.js');
 
+// Note: RFC 9110 Section 9.3.1. GET specifies that GET requests should not make changes in the server state (e.g., generate content).
+// https://www.rfc-editor.org/rfc/rfc9110.html#name-get
+
 router.post('/login', accountController.login);
 router.post('/editadminpassword', accountController.editAdminPassword);
 router.post('/createbranch', accountController.createBranch);
@@ -17,7 +20,7 @@ router.post('/editsales', branchController.editSales)
 router.post('/editexpense', branchController.editExpense)
 router.post('/deletesales', branchController.deleteSales);
 router.post('/deleteexpense', branchController.deleteExpense);
-router.get('/submit', branchController.submitSalesAndExpenses);
+router.get('/submit', branchController.submitSalesAndExpenses);  //FIXME: Change this to POST
 
 router.post('/adminaddsales', adminController.adminAddSales);
 router.post('/adminaddexpense', adminController.adminAddExpense);
@@ -33,7 +36,7 @@ router.post('/addcheque', adminController.addCheque);
 router.get('/viewcheque', adminController.viewCheque);
 router.post('/editcheque', adminController.editCheque);
 router.post('/deletecheque', adminController.deleteCheque);
-router.post('/genreport', adminController.generateReport)
-router.post('/genqrtr', adminController.generateQuarterlyReport);
+router.post('/genreport', adminController.generateReport)  //FIXME: Can this be changed to GET?
+router.post('/genqrtr', adminController.generateQuarterlyReport);  //FIXME: Can this be changed to GET?
 
 module.exports = router;
