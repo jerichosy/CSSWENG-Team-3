b9e9b849775d0bca4de774bb5554f6a4e2a1d3cc
andres-dalisay <apadx3304@gmail.com>
2022-12-07 13:07:13 +0800

    designed navbar for cashier side

--- panay-app/src/components/cashier/MobileNav.vue
+++ panay-app/src/components/cashier/MobileNav.vue
@@ -3,25 +3,30 @@
 </script>
 
 <template>
-    <nav id="mobile-nav" class="nav fixed-bottom bg-white">
-        <div class="container-fluid">
-            <a href="">
-                <i class="material-icons">
-                    home
-                </i>
-                <span>Home</span>
-            </a>
-            <a href="">
-                <span>Sales</span>
-            </a>
-            <a href="">
-                <span>Expenses</span>
-            </a>
-            <a href="">
-                <span>Review</span>
-            </a>
-        </div>
-    </nav>
+    <ul id="mobile-nav" class="nav nav-fill fixed-bottom justify-content-center btn-group bg-white">
+        <li class="nav-item btn btn-outline-primary">
+            <i class="material-icons">
+                home
+            </i>
+            <br />
+            <span>Home</span>
+        </li>
+        <li class="nav-item btn btn-outline-primary">
+            <i class="material-icons">money</i>
+            <br />
+            <span>Sales</span>
+        </li>
+        <li class="nav-item btn btn-outline-primary">
+            <i class="material-icons">account_balance_wallet</i>
+            <br />
+            <span>Expenses</span>
+        </li>
+        <li class="nav-item btn btn-outline-primary">
+            <i class="material-icons">check_circle_outline</i>
+            <br />
+            <span>Review</span>
+        </li>
+    </ul>
 </template>
 
 <style>
--- panay-app/src/main.js
+++ panay-app/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import 'material-icons/iconfont/material-icons.css';
 
 import '../scss/style.css'
 
