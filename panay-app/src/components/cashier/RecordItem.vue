f86f58f57ddede8d51dbe9d5cd50dc2d96e0d28c
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-12-11 12:38:17 +0800

    added initial cashier expenses view

--- panay-app/src/components/cashier/RecordItem.vue
+++ panay-app/src/components/cashier/RecordItem.vue
@@ -36,6 +36,32 @@ export default {
                     </div>
                 </div>
             </template>
+
+            <template v-if="recordType === 'expenses'">
+                <div class="col">
+                    <div class="row-cols-1 text-start">
+                        <div class="col h4">
+                            Category
+                        </div>
+                        <div class="col h6">
+                            Item Name
+                        </div>
+                        <div class="col h6">
+                            Description
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="row-cols-1 text-end">
+                        <div class="col h4">
+                            P500.00
+                        </div>
+                        <div class="col h6">
+                            (P500.00)
+                        </div>
+                    </div>
+                </div>
+            </template>
         </div>
     </div>
 
--- panay-app/src/views/cashier/CashierExpenses.vue
+++ panay-app/src/views/cashier/CashierExpenses.vue
@@ -1,6 +1,32 @@
 <script>
+import RecordItem from '../../components/cashier/RecordItem.vue'
+
+export default {
+    components: {
+        RecordItem
+    }
+}
 </script>
 
 <template>
-    cashier expenses
+    <div id="expenses-container" class="container">
+        <div class="row justify-content-center p-2">
+            <div class="col my-auto">
+                <h1>Expenses</h1>
+            </div>
+            <div class="col text-center my-auto">
+                <p class="h5 align-middle">Total Expenses: P1,400.00</p>
+            </div>
+            <div class="col my-auto text-end">
+                <button class="btn btn-primary">Add</button>
+            </div>
+        </div>
+
+        <div class="row-cols-1 m-1 justify-content-center">
+            <RecordItem record-type="expenses" />
+            <RecordItem record-type="expenses" />
+            <RecordItem record-type="expenses" />
+        </div>
+
+    </div>
 </template>
