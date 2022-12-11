f3f7bcbdb7f4c0745a3df797ee4c0a4fc9bfdd55
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-12-11 13:25:15 +0800

    removed Categories link in nav

--- panay-app/src/components/admin/AdminNav.vue
+++ panay-app/src/components/admin/AdminNav.vue
@@ -52,18 +52,6 @@ import { RouterLink, RouterView } from 'vue-router'
                 </RouterLink>
             </li>
             <li class="nav-item">
-                <RouterLink class="nav-link d-flex" to="/admin/categories">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="feather feather-archive">
-                        <polyline points="21 8 21 21 3 21 3 8"></polyline>
-                        <rect x="1" y="3" width="22" height="5"></rect>
-                        <line x1="10" y1="12" x2="14" y2="12"></line>
-                    </svg>
-                    <span class="ps-2">Categories</span>
-                </RouterLink>
-            </li>
-            <li class="nav-item">
                 <RouterLink class="nav-link d-flex" to="/admin/account">
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
