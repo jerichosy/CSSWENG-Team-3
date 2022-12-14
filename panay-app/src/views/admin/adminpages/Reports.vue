0d8d914930497f28276487e76627e7b10995f774
andres-dalisay <andres_dalisay@dlsu.edu.ph>
2022-12-14 12:21:08 +0800

    renamed filenames

--- panay-app/src/views/admin/adminpages/Reports.vue
+++ panay-app/src/views/admin/adminpages/Reports.vue
@@ -43,7 +43,7 @@ export default {
                     // create "a" HTML element with href to file & click
                     const link = document.createElement('a');
                     link.href = href;
-                    link.setAttribute('download', 'file.xlsx'); //or any other extension
+                    link.setAttribute('download', 'Monthly Report.xlsx'); //or any other extension
                     document.body.appendChild(link);
                     link.click();
 
@@ -76,7 +76,7 @@ export default {
                     // create "a" HTML element with href to file & click
                     const link = document.createElement('a');
                     link.href = href;
-                    link.setAttribute('download', 'file.xlsx'); //or any other extension
+                    link.setAttribute('download', 'Quarterly Report.xlsx'); //or any other extension
                     document.body.appendChild(link);
                     link.click();
 
